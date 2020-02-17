badfb25bd84c9ed8518978c92409680d9530ff88
xula <1379960679@qq.com>
2020-02-17 21:16:58 +0800

    添加 word转pdf 功能

--- src/main/java/com/rjgf/poi/word/util/WordUtil.java
+++ src/main/java/com/rjgf/poi/word/util/WordUtil.java
@@ -5,10 +5,11 @@ import com.deepoove.poi.data.MiniTableRenderData;
 import com.deepoove.poi.data.PictureRenderData;
 import com.deepoove.poi.data.RowRenderData;
 import com.deepoove.poi.data.TextRenderData;
+import fr.opensagres.poi.xwpf.converter.pdf.PdfConverter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,7 +26,7 @@ import java.util.Map;
 public class WordUtil {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Map<String, Object> map = new HashMap<>();
         map.put("province", "浙江省");
         map.put("city", "金华市");
@@ -41,15 +42,18 @@ public class WordUtil {
         String filePath = "E:\\Users\\xulia\\润建\\app 大数据\\word 模块\\";
         String tmpFileName = "template.docx";
         String fileName = "out_template.docx";
-        WordUtil.genFile(filePath,tmpFileName,fileName,map);
+        WordUtil.genFile(filePath, tmpFileName, fileName, map);
+
+
+        wordToPdf("e://test.docx","e://test1.pdf");
     }
 
 
     /**
-     * @param filePath     文件路径
+     * @param filePath    文件路径
      * @param tmpFileName 模板文件名称
-     * @param fileName     导出的文件名
-     * @param data         代替的数据
+     * @param fileName    导出的文件名
+     * @param data        代替的数据
      */
     public static void genFile(String filePath, String tmpFileName, String fileName, Map<String, Object> data) {
         XWPFTemplate template = XWPFTemplate.compile(filePath + tmpFileName).render(data);
@@ -71,6 +75,43 @@ public class WordUtil {
             if (out != null) {
                 try {
                     out.flush();
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    /**
+     * word 转 pdf
+     *
+     * @param wordInputFile
+     * @param pdfOutputFile
+     * @throws IOException
+     */
+    public static void wordToPdf(String wordInputFile, String pdfOutputFile) {
+        FileInputStream in = null;
+        OutputStream out = null;
+        try {
+            in = new FileInputStream(wordInputFile);
+            XWPFDocument document = new XWPFDocument(in);
+            File outFile = new File(pdfOutputFile);
+            out = new FileOutputStream(outFile);
+            PdfConverter.getInstance().convert(document, out, null);
+        } catch (Exception e) {
+          log.error("word 转 pdf 失败！",e);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (out != null) {
+                try {
                     out.close();
                 } catch (IOException e) {
                     e.printStackTrace();
